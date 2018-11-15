d32999d893ef81d109faba7216c2ba94da2ca60c
Tyler Edelen <tedelen@conductor.com>
2018-11-14 22:54:40 -0500

    use redis user auth store

--- big-2-api/src/main/java/com/gmo/big2/api/config/AuthConfig.java
+++ big-2-api/src/main/java/com/gmo/big2/api/config/AuthConfig.java
@@ -4,21 +4,25 @@ import java.time.Duration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import com.gmo.big.two.auth.api.JsonWebTokenAuthenticationProvider;
 import com.gmo.big.two.auth.impl.StaticJsonWebTokenAuthenticationProvider;
 import com.gmo.big.two.auth.store.InMemoryUserAuthStore;
+import com.gmo.big.two.auth.store.RedisUserAuthStore;
 import com.gmo.big.two.auth.store.UserAuthStore;
 import com.gmo.big.two.auth.utils.JwtUtils;
+import redis.clients.jedis.ShardedJedisPool;
 
 /**
  * Configuration for auth-related beans
  */
 @Configuration
+@Import(RedisStoreConfig.class)
 public class AuthConfig {
     @Bean
-    public UserAuthStore userAuthStore() {
-        return new InMemoryUserAuthStore();
+    public UserAuthStore userAuthStore(final ShardedJedisPool gameJedisDb) {
+        return new RedisUserAuthStore(gameJedisDb);
     }
 
     @Bean
